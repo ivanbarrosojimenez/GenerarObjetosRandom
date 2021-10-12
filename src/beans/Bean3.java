10aa47462428193a445b3ac692b016d7b2410c40
ibarrosj <ibarrosj>
2021-10-12 12:59:42 +0200

    test Map

--- src/beans/Bean3.java
+++ src/beans/Bean3.java
@@ -8,9 +8,21 @@ public class Bean3 {
 	private String string4 = null;
 	private int int5 = 0;
 	private Double double6 = new Double(0);
-	private Double double7 =null;
+	private Double double7 = null;
 	private List<Integer> listInteger;
 	private List<Bean1> listBean1;
+	private Map<Integer, String> map;
+	private Map<Integer, Bean1> map2;
+
+	
+	
+	public Map<Integer, Bean1> getMap2() {
+		return map2;
+	}
+
+	public void setMap2(Map<Integer, Bean1> map2) {
+		this.map2 = map2;
+	}
 
 	public List<Bean1> getListBean1() {
 		return listBean1;
@@ -20,9 +32,6 @@ public class Bean3 {
 		this.listBean1 = listBean1;
 	}
 
-
-	private Map<Integer, String> map;
-	
 	public Map<Integer, String> getMap() {
 		return map;
 	}
@@ -35,79 +44,58 @@ public class Bean3 {
 		return listInteger;
 	}
 
-
 	public void setListInteger(List<Integer> listInteger) {
 		this.listInteger = listInteger;
 	}
 
-
 	public Bean3() {
 	}
-
 
 	public String getString3() {
 		return string3;
 	}
 
-
 	public void setString3(String string3) {
 		this.string3 = string3;
 	}
-
 
 	public String getString4() {
 		return string4;
 	}
 
-
 	public void setString4(String string4) {
 		this.string4 = string4;
 	}
-
 
 	public int getInt5() {
 		return int5;
 	}
 
-
 	public void setInt5(int int5) {
 		this.int5 = int5;
 	}
-
 
 	public Double getDouble6() {
 		return double6;
 	}
 
-
 	public void setDouble6(Double double6) {
 		this.double6 = double6;
 	}
-
 
 	public Double getDouble7() {
 		return double7;
 	}
 
-
 	public void setDouble7(Double double7) {
 		this.double7 = double7;
 	}
-
 
 	@Override
 	public String toString() {
 		return "Bean3 [string3=" + string3 + ", string4=" + string4 + ", int5=" + int5 + ", double6=" + double6
 				+ ", double7=" + double7 + ", listInteger=" + listInteger + ", listBean1=" + listBean1 + ", map=" + map
-				+ "]";
+				+ ", map2=" + map2 + "]";
 	}
 
-
-
-
-	
-	
-
-	
-	
 }
